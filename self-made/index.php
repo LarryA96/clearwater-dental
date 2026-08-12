238f1ceed2b682300365934a217e28141f1494ea
LarryA96 <156938571+LarryA96@users.noreply.github.com>
2026-08-12 16:23:24 -0400

    Create basic HTML structure for index.php

--- self-made/index.php
+++ self-made/index.php
@@ -1,1 +1,15 @@
-//Home page will be the demographic search
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clearwater Dental</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <?php require 'header.php'; ?>
+  <?php require 'footer.php'; ?>
+</body>
+</html>
