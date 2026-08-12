bc9fabdcaad29587f78a818fbe1c32e7304b8d29
LarryA96 <156938571+LarryA96@users.noreply.github.com>
2026-08-12 16:33:34 -0400

    Refactor menu links into a nav element

--- self-made/header.php
+++ self-made/header.php
@@ -7,21 +7,22 @@
 
   <!-- Main menu with navigation links -->
   <div class="menu">
-      
-      <a href="details.php" class="menu-button">
-        Search Patient Details
-      </a>
-
-      <a href="history.php" class="menu-button">
-        Search Patient History
-      </a>
-      
-      <a href="add_patient.php" class="menu-button">
-        Add New Patient
-      </a>
-      
-      <a href="add_event.php" class="menu-button">
-        Add Patient Event
-      </a>
+      <nav>
+        <a href="details.php" class="menu-button">
+          Search Patient Details
+        </a>
+  
+        <a href="history.php" class="menu-button">
+          Search Patient History
+        </a>
+        
+        <a href="add_patient.php" class="menu-button">
+          Add New Patient
+        </a>
+        
+        <a href="add_event.php" class="menu-button">
+          Add Patient Event
+        </a>
+      </nav>
   </div>
 </header>
