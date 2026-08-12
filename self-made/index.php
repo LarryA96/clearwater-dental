5b7511ce27a61f8c5c6bedd9dbf98ea428873d21
LarryA96 <156938571+LarryA96@users.noreply.github.com>
2026-08-12 16:31:18 -0400

    Refactor index.php to include a container div
    Added a container div to the body for layout purposes.

--- self-made/index.php
+++ self-made/index.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Clearwater Dental</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-  <?php require 'header.php'; ?>
-  <?php require 'footer.php'; ?>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Clearwater Dental</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    
+    <body>
+        <div class="container">
+          <?php require 'header.php'; ?>
+          <?php require 'footer.php'; ?>
+        </div>
+    </body>
 </html>
