fabba3ddd5465c967939f57750c942ebfcbf63b1
LarryA96 <156938571+LarryA96@users.noreply.github.com>
2026-08-12 16:12:47 -0400

    Add header section with navigation links

--- self-made/header.php
+++ self-made/header.php
@@ -1,1 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<div class="container">
+  <header>
+    
+    <!-- Website title -->
+    <h1>Clearwater Dental</h1>
+    <p>Patient Management System</p>
 
+    <!-- Main menu with navigation links -->
+    <div class="menu">
+      
+        <a href="index.php" class="menu-button">
+          Search Patient Details
+        </a>
+
+        <a href="history.php" class="menu-button">
+          Search Patient History
+        </a>
+      
+        <a href="add_patient.php" class="menu-button">
+          Add New Patient
+        </a>
+      
+        <a href="add_event.php" class="menu-button">
+          Add Patient Event
+        </a>
+    </div>
+  </header>
+</div>
